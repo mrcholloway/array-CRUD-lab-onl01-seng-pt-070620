155b7c458707b9c6a685452ed575032702fc3d07
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-19 22:16:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["dog", "cat", "bird", "pig"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array_element.push("chicken")
 end
 
 def add_element_to_start_of_array(array, element)
